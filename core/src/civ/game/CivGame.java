065855d7c920b8e73e61dbeb8a15cf1acac12e78
Badr_Bikria <bikriabadr@gmail.com>
2024-04-18 16:09:56 +0100

    Ajout du menu pop-up

---
+++ core/src/civ/game/CivGame.java
@@ -0,0 +1,32 @@
+package civ.game;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+
+
+public class CivGame extends Game {
+
+    Stage stage;
+    SpriteBatch tile_batch;
+    SpriteBatch character_batch;
+    public BitmapFont font;
+
+    public void create() {
+
+        stage = new Stage();
+        font = new BitmapFont(); // use libGDX's default Arial font
+        this.setScreen(new EcranJeu(this));
+    }
+
+    public void render() {
+        super.render(); // important!
+    }
+
+    public void dispose() {
+        stage.dispose();
+        font.dispose();
+    }
+
+}
--- desktop/src/civ/game/DesktopLauncher.java
+++ desktop/src/civ/game/DesktopLauncher.java
@@ -2,7 +2,7 @@ package civ.game;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import civ.game.civgame;
+import civ.game.CivGame;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
@@ -11,6 +11,6 @@ public class DesktopLauncher {
 		config.setForegroundFPS(60);
 		config.setWindowedMode(600, 400);
 		config.setTitle("civgame");
-		new Lwjgl3Application(new civgame(), config);
+		new Lwjgl3Application(new CivGame(), config);
 	}
 }
