622027519d4f7e2456f0dbf61b3f143f93d4601e
Maxime BONCOUR <max.boncour@gmail.com>
2024-05-22 00:47:51 +0200

    remove unnecessary console output

--- core/src/com/javaciv/gameElement/City.java
+++ core/src/com/javaciv/gameElement/City.java
@@ -87,7 +87,6 @@ public class City {
         for(int i = -1; i <= 1; i++) {
             for(int j = -1; j <= 1; j++) {
                 if(i == 0 && j == 0) continue; // Skip the center tile
-                System.out.println("i : " + i + " j : " + j);
                 Tile newTile = this.owner.getWorldMap().at(x + i, y + j);
                 this.cityTiles.add(newTile);
                 addNeighbourTiles(newTile);
